7170e58c4775c2fcb3673ee2d491b1254734dbb6
stephane bard <stephane.bard@gmail.com>
2014-05-15 21:57:00 +0200

    fix delete project

--- hg_delivery/static/main.js
+++ hg_delivery/static/main.js
@@ -47,14 +47,14 @@ function go_to(url) {
  *
  */
 function delete_this_project(id_project){
-  var $button = $(this);
-  var target_url = $button.data('url');
+  var $button = $('#view_delete_project');
+  var target_url = $button.data('url')+id_project;
   $.ajax({url:target_url,
           success:function(json_response){
             if(json_response.result){
               // fallback to root
               go_to('/');
-              return false;
+              return true;
             }
           } 
          });
