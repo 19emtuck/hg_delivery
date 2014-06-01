3efb8407fef502d3f744ab4f1907bb3be62ef071
stephane bard <stephane.bard@gmail.com>
2014-06-01 12:43:36 +0200

    fix template

--- hg_delivery/nodes.py
+++ hg_delivery/nodes.py
@@ -116,7 +116,7 @@ class HgNode(NodeSsh):
       :param nb_lines: integer, limit the number of lines
     """
 
-    __template = "{node}||#|{author}|#|{branches}|#|{rev}|#|{parents}|#|{desc|jsonescape}|#|{tags}\n" 
+    __template = "{node}|#|{author}|#|{branches}|#|{rev}|#|{parents}|#|{desc|jsonescape}|#|{tags}\n" 
 
     try :
       if revision_filter :
