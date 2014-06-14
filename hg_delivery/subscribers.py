b776c6644f964ff1d5fb6d1d4cfba1a3f8d4f412
stephane bard <stephane.bard@gmail.com>
2014-06-14 16:29:14 +0200

    add new subscriber to log commands

--- hg_delivery/subscribers.py
+++ hg_delivery/subscribers.py
@@ -7,6 +7,7 @@
 # hg_delivery is free software; you can redistribute it and/or modify it under the
 # terms of the M.I.T License.
 #
+
 from pyramid.events import (
      NewRequest,
      NewResponse,
@@ -33,7 +34,10 @@ def mysubscriber(event):
 @subscriber(NewResponse)
 def log_ssh_commands(event):
   """
+    at the end of each response we check
+    if we need to log something
   """
-  for __command in NodeSsh.logs :
-    DBSession.add(RemoteLog(command = __command))
-  del NodeSsh.logs[0:]
+  if NodeSsh.logs :
+    for __command in NodeSsh.logs :
+      DBSession.add(RemoteLog(command = __command))
+    del NodeSsh.logs[0:]
