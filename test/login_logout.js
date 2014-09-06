a4c6b5cf155cb3fac1cce48ef06d6ce317125aef
stephane bard <stephane.bard@gmail.com>
2014-09-06 12:35:15 +0200

    add some tests

---
+++ test/login_logout.js
@@ -0,0 +1,42 @@
+casper.userAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X)');
+casper.start();
+
+// fix view port to get large screenshot image
+casper.viewport(1024, 768);
+
+
+// some configuration events ...
+casper.on('step.start', function(){
+   this.evaluate(function(){document.body.bgColor = 'white';});
+});
+casper.on('step.complete', function(){
+  if(!casper.cli.has('fast')){
+    this.capture('images/test_'+casper.step+'.jpg', undefined,{ format:'jpg', quality:100});
+  }
+});
+casper.on('step.error', function(error){
+  if(!casper.cli.has('fast')){
+    this.capture('images/error_'+casper.step+'.jpg', undefined,{ format:'jpg', quality:100});
+  }
+});
+casper.on('remote.message',function(message){this.echo(message)});
+
+casper.thenOpen('http://127.0.0.1:6543');
+casper.then(function(response){ this.test.assertTitle('Hg Delivery 1.0'); });
+casper.then(function(response){
+  this.fill('#login_form', {'login':'editor','password':'editor'});
+  this.click('#log_me')
+});
+casper.then(function(response){
+  this.test.assertTitle('Hg Delivery 1.0');
+  this.test.assertTextExists('Dashboard');
+  this.test.assertExists('span[class="glyphicon glyphicon-plus"]');
+});
+
+casper.thenClick("#sign_out");
+casper.then(function(response){
+  this.test.assertTitle('Hg Delivery 1.0');
+  this.test.assertTextDoesntExist('Dashboard');
+  this.test.assertDoesntExist('span[class="glyphicon glyphicon-plus"]');
+});
+casper.run();
---
+++ test/project.js
@@ -0,0 +1,75 @@
+casper.userAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X)');
+casper.start();
+
+// fix view port to get large screenshot image
+casper.viewport(1024, 768);
+
+
+// some configuration events ...
+casper.on('step.start', function(){
+   this.evaluate(function(){document.body.bgColor = 'white';});
+});
+casper.on('step.complete', function(){
+  if(!casper.cli.has('fast')){
+    this.capture('images/test_'+casper.step+'.jpg', undefined,{ format:'jpg', quality:100});
+  }
+});
+casper.on('step.error', function(error){
+  if(!casper.cli.has('fast')){
+    this.capture('images/error_'+casper.step+'.jpg', undefined,{ format:'jpg', quality:100});
+  }
+});
+casper.on('remote.message',function(message){this.echo(message)});
+
+casper.thenOpen('http://127.0.0.1:6543');
+casper.then(function(response){ this.test.assertTitle('Hg Delivery 1.0'); });
+casper.then(function(response){
+  this.fill('#login_form', {'login':'editor','password':'editor'});
+  this.click('#log_me')
+});
+casper.then(function(response){
+  this.test.assertTitle('Hg Delivery 1.0');
+  this.test.assertTextExists('Dashboard');
+  this.test.assertExists('span[class="glyphicon glyphicon-plus"]');
+});
+
+// add a new project
+casper.then(function(response){ this.click('span[class="glyphicon glyphicon-plus"]') });
+casper.then(function(response){ this.test.assertExists('#add_my_project'); });
+casper.then(function(response){
+  this.fill('form[name="project"]', {'name':'t8',
+	                 'host':'127.0.0.1',
+	                 'path':'/home/sbard/dev/t8',
+                         'user':'sbard',
+                         'password':'evangelion',
+                        });
+});
+casper.thenClick('#add_my_project');
+casper.waitWhileVisible('#new_project_dialog');
+casper.waitUntilVisible('.alert-success');
+
+// try to add the same project twice !
+casper.then(function(response){ this.click('span[class="glyphicon glyphicon-plus"]') });
+casper.then(function(response){ this.test.assertExists('#add_my_project'); });
+casper.then(function(response){
+  this.fill('form[name="project"]', {'name':'t8',
+	                 'host':'127.0.0.1',
+	                 'path':'/home/sbard/dev/t8',
+                         'user':'sbard',
+                         'password':'evangelion',
+                        });
+});
+casper.thenClick('#add_my_project');
+casper.waitUntilVisible('.alert-danger');
+casper.thenClick("#cancel_add_project");
+casper.waitWhileVisible('#new_project_dialog');
+
+casper.thenClick('#projects_list');
+
+casper.then(function(response){
+  this.test.assertTextExists('t8');
+});
+
+// then we delete the project
+
+casper.run();
