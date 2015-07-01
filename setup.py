6c70a7c01f3889cff12bf5c31104f81a5a55eaa7
stephane bard <stephane.bard@gmail.com>
2015-07-01 10:39:55 +0200

    change release number for delivery fix

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ requires = [
     ]
 
 setup(name='hg_delivery',
-      version='0.7',
+      version='0.7.1',
       description=description,
       long_description=README + '\n\n' + CHANGES,
       classifiers=classifiers,
