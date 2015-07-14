cf863a342f1edccee24c6a50f70a5a0e2fafab5a
stephane bard <stephane.bard@gmail.com>
2015-07-14 22:27:57 +0200

    update setup for pypi

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ requires = [
     ]
 
 setup(name='hg_delivery',
-      version='0.7.2',
+      version='0.7.3',
       description=description,
       long_description=README + '\n\n' + CHANGES,
       classifiers=classifiers,
