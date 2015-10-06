f4d47a83738f14f49748e1bf2cc4d888d91062e6
stephane bard <stephane.bard@gmail.com>
2015-10-06 20:06:26 +0200

    update setup to prepare relase

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ requires = [
     ]
 
 setup(name='hg_delivery',
-      version='0.7.4',
+      version='0.7.5',
       description=description,
       long_description=README + '\n\n' + CHANGES,
       classifiers=classifiers,
