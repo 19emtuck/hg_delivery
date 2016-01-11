73c7f5cab77d83aa8b53ea5facbf3d26963d0758
stephane bard <stephane.bard@gmail.com>
2016-01-11 21:12:32 +0100

    update setup to prepare new tag

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ requires = [
     ]
 
 setup(name='hg_delivery',
-      version='0.8.4',
+      version='0.8.5',
       description=description,
       long_description=README + '\n\n' + CHANGES,
       classifiers=classifiers,
