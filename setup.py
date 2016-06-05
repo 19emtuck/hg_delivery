0fdc2b58a179cd0ede77b99818b8fcd111d84b6a
stephane bard <stephane.bard@gmail.com>
2016-06-05 14:41:15 +0200

    count++

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ requires = [
     ]
 
 setup(name='hg_delivery',
-      version='0.9.1',
+      version='0.9.2',
       description=description,
       long_description=README + '\n\n' + CHANGES,
       classifiers=classifiers,
