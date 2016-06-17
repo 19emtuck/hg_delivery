b308e536d3ccfc380451cc369c9b43423863e688
stephane bard <stephane.bard@gmail.com>
2016-06-17 19:07:45 +0200

    prepare new release

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ requires = [
     ]
 
 setup(name='hg_delivery',
-      version='0.9.2',
+      version='0.9.3',
       description=description,
       long_description=README + '\n\n' + CHANGES,
       classifiers=classifiers,
