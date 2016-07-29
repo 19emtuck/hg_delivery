1a1c18defde6a404f5ecb16bb411b1ff54cfc862
stephane bard <stephane.bard@gmail.com>
2016-07-29 22:33:15 +0200

    change setup to be released on pypi

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ requires = [
     ]
 
 setup(name='hg_delivery',
-      version='1.0.0',
+      version='1.0.1',
       description=description,
       long_description=README + '\n\n' + CHANGES,
       classifiers=classifiers,
